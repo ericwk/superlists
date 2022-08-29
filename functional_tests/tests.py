a05b05f31fcb8fcb0c5e019e729aab32f8e2feb8
Eric Kleinschmidt <ericwk@comcast.net>
2022-08-29 17:22:15 -0500

    Make functional_tests an app, use LiveServerTestCase to enable internal Django test cleanup.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,12 @@
 import time
-import unittest
+
+from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -19,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -38,6 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
@@ -48,8 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a FLY')
 
         # Edith wonders whether the site will remember her list.  Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -58,6 +61,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # She visits the URL - her to-do list is still there.
         # Satisfied, she goes back to sleep.
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
